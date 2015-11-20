6b5cb4d8d7c2413eae03efafde517945bbc9952e
Sebastian Golasch <public@asciidisco.com>
2015-11-20 13:57:52 +0100

    feat(log): Add logging via morgan

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-#! /usr/bin/env node
 var spdy = require('spdy');
 var keys = require('spdy-keys');
 var ecstatic = require('ecstatic');
-var fs = require('fs');
-var port = process.argv[2] || 5000;
- 
-var options = {
-  key: keys.key,
-  cert: keys.cert,
-  ca: keys.ca
+var morgan = require('morgan');
+
+module.exports = function (options, cb) {
+  options = options || {};
+
+  // determine port & keys
+  var port = options.port || 5000;
+  var spdyOpts = options.spdy || {
+    key: keys.key,
+    cert: keys.cert,
+    ca: keys.ca
+  };
+
+  // create logger "middleware"
+  var logger = morgan('combined');
+
+  // create static server "middleware"
+  var es = ecstatic({root: process.cwd() + '/'});
+
+  // 
+  var middleware = function (req, res) {
+    logger(req, res, function (err) {
+      if (err) console.error(err);
+      es(req, res);
+    });
+  };
+
+  var server = spdy.createServer(spdyOpts, middleware);
+  server.listen(port, cb.bind(server, port) || function () {});
+  return server;
 };
- 
-var server = spdy.createServer(options, ecstatic({root: process.cwd() + '/'}));
-server.listen(port, function () {
-  console.log('Server listening at: https://localhost:%s', port);
-});
