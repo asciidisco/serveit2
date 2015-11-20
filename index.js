fb1810d816b3869f125775cbfa6a77a618f49db7
Sebastian Golasch <public@asciidisco.com>
2015-11-20 18:39:46 +0100

    feat(core): Add server push support

--- index.js
+++ index.js
@@ -2,33 +2,70 @@ var spdy = require('spdy');
 var keys = require('spdy-keys');
 var ecstatic = require('ecstatic');
 var morgan = require('morgan');
+var mime = require('mime');
+var fs = require('fs');
 
 module.exports = function (options, cb) {
   options = options || {};
+  cb = typeof options === 'function' ? options : cb;
 
-  // determine port & keys
+  // determine port, keys & root folder
   var port = options.port || 5000;
-  var spdyOpts = options.spdy || {
+  var root = options.root || process.cwd() + '/';
+  if (root[root.length-1] !== '/') {
+    root += '/';
+  }
+
+  // add ssl keys
+  // TODO: Enable users to add their own keys
+  var spdyOpts = {
     key: keys.key,
     cert: keys.cert,
-    ca: keys.ca
+    ca: keys.ca,
+  };
+
+  // dummy `logger` that prevents the script from outputting anything
+  var quietLogger = function (req, res, _cb) {
+    _cb();
   };
 
   // create logger "middleware"
-  var logger = morgan('combined');
+  var logger = options.quiet ? quietLogger : morgan('dev');
 
   // create static server "middleware"
-  var es = ecstatic({root: process.cwd() + '/'});
+  var es = ecstatic({root: root});
 
-  // 
+  // default middleware (logging + static file server)
   var middleware = function (req, res) {
+    var requestedFile = req.url.replace('/', '');
+    if (requestedFile === '') requestedFile = 'index.html';
+
+    // push resources
+    if (options.push && options.push[requestedFile] && options.push[requestedFile].length > 0) {
+      options.push[requestedFile].forEach(function (file) {
+        var stream = res.push(file, {
+          request: { accept: '*/*'},
+          response: {'content-type': mime.lookup(file)},
+        });
+        stream.on('error', console.error);
+        try {
+          var pushable = fs.readFileSync(root + file);
+          stream.end(pushable);
+          if (!options.quiet) console.log('PUSH', '/' + file, '-', pushable.length);
+        } catch (e) {
+          console.error('Error pushing file:', root + file, e);
+        }
+      });
+    }
+
+
     logger(req, res, function (err) {
       if (err) console.error(err);
-      es(req, res);
+      es(req, res, typeof options.middleware === 'function' || function () {});
     });
   };
 
   var server = spdy.createServer(spdyOpts, middleware);
-  server.listen(port, cb.bind(server, port) || function () {});
+  server.listen(port, typeof cb === 'function' ? cb.bind(server, port) : function () {});
   return server;
 };
